789fdcf2c4c51e4907f2798e37f301b54e452e14
Wlad loban <qwegadqwe@gmail.com>
2022-06-02 00:54:26 +0300

    HW2:props

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[]
+    setFilter: (filter: FilterType) => void            //React.Dispatch<React.SetStateAction<FilterType>>
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -17,10 +17,22 @@ function Affairs(props: AffairsPropsType) {
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => {
+        props.setFilter('all')
+        console.log('a')
+    } // need to fix
+    const setHigh = () => {
+        props.setFilter('high')
+        console.log('h')
+    }
+    const setMiddle = () => {
+        props.setFilter('middle')
+        console.log('m')
+    }
+    const setLow = () => {
+        props.setFilter('low')
+        console.log('l')
+    }
 
     return (
         <div>
